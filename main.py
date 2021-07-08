b67e8e8015ecef2ae147181c0ad887727a20ec91
Raidez <alexis.lucien@outlook.fr>
2021-07-08 10:41:43 +0200

    Ajout des ressources Ajout des libs 1er code pour faire tourner Ursina

---
+++ main.py
@@ -0,0 +1,14 @@
+from ursina import *
+
+app = Ursina()
+ground = Entity(
+    model = 'cube',
+    color = color.magenta,
+    z = -.1,
+    y = -3,
+    origin = (0, .5),
+    scale = (50, 1, 10),
+    collider = 'box'
+)
+
+app.run()
